a5ef1c5b81a37869fb76dbe6bb85431a72845323
Povilas Carev <povilas.ca@gmail.com>
2025-11-28 14:38:56 +0200

    Added tailwind to the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,17 @@
-import Home from "./pages/Home";
-import Settings from "./pages/Settings";
-import CatalogProducts from "./pages/CatalogProducts";
-import TaxesAndServiceCharges from "./pages/TaxesAndServiceCharges";
-import UsersAndRoles from "./pages/UsersAndRoles";
-import Payments from "./pages/Payments";
-import Reservations from "./pages/Reservations";
-import Navbar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
-
-export default function App() {
+function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Navbar />
-
-    <div className="p-6">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/catalog-products" element={<CatalogProducts />} />
-        <Route path="/catalog-products" element={<CatalogProducts />} />
-        <Route path="/taxes-and-service-charges" element={<TaxesAndServiceCharges />} />
-        <Route path="/users-and-roles" element={<UsersAndRoles />} />
-        <Route path="/payments" element={<Payments />} />
-        <Route path="/reservations" element={<Reservations />} />
-      </Routes>
-      </div>
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is working! 
+      </h1>
+      <p className="mt-4 text-gray-500">
+        Chill Kmr
+      </p>
+      <button className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+        Click Me
+      </button>
     </div>
   );
 }
+
+export default App;
