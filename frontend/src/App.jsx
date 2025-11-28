da8064e8e6436dc908f1b2b5bf16e4fa7e00f05d
Povilas Carev <povilas.ca@gmail.com>
2025-11-28 14:38:56 +0200

    Added tailwind to the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,15 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [weather, setWeather] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5168/weatherforecast")
-      .then(res => res.json())
-      .then(data => setWeather(data))
-      .catch(err => console.error(err));
-  }, []);
-
   return (
-    <div>
-      <h1>Weather forecast:</h1>
-      <p>
-        {weather.map((w, index) => (
-          <li key={index}>
-            {w.date} – {w.summary} – {w.temperatureC}°C
-          </li>
-        ))}
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is working! 
+      </h1>
+      <p className="mt-4 text-gray-500">
+        Chill Kmr
       </p>
+      <button className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+        Click Me
+      </button>
     </div>
   );
 }
